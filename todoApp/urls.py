4018a33a66b554e8e7a02b606f544db7cb681fad
divyansh <kushwaha.divyansh.dxn@gmail.com>
2021-06-16 22:25:48 +0530

    removed useless codes, printing the document still missing

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('',views.index),
     path('toggleStatus/',views.toggleStatus),
     path('<int:todoId>/deleteTodo',views.deleteTodo,name='deleteTodo'),
-    path('print/',views.write_pdf_view,name='print')
+    # path('print/',views.print,name='print')
 ]
